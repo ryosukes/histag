2bd0d212a46cda5962f4215a14ef32a3cd73ac1f
Ryosuke Sasaki <nu.ciger@gmail.com>
2017-09-22 16:50:29 +0900

    fix working dir and format

--- cmd/root.go
+++ cmd/root.go
@@ -1,31 +1,30 @@
 package cmd
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/spf13/cobra"
-    "github.com/spf13/viper"
+	"github.com/spf13/cobra"
 )
 
 var RootCmd = &cobra.Command{
-    Use:   "histag",
-    Short: "This tool is pretty cool.",
-    Long:  "This tool is a great convenience.",
-    Run: func(cmd *cobra.Command, args []string) {
+	Use:   "histag",
+	Short: "This tool is pretty cool.",
+	Long:  "This tool is a great convenience.",
+	Run: func(cmd *cobra.Command, args []string) {
 
-    },
+	},
 }
 
 func init() {
-    cobra.OnInitialize()
-    RootCmd.AddCommand(versionCmd)
+	cobra.OnInitialize()
+	RootCmd.AddCommand(versionCmd)
 }
 
 var versionCmd = &cobra.Command{
-    Use:   "version",
-    Short: "Print the version number of histag",
-    Long:  `All software has versions. This is histag's`,
-    Run: func(cmd *cobra.Command, args []string) {
-        fmt.Println("histag v1.0")
-    },
+	Use:   "version",
+	Short: "Print the version number of histag",
+	Long:  `All software has versions. This is histag's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("histag v1.0")
+	},
 }
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "histag/cmd"
+import (
+	"fmt"
+	"os"
+
+	"histag/cmd"
+)
 
 func main() {
-    if err := cmd.RootCmd.Execute(); err != nil {
-        fmt.Println(err)
-        os.Exit(-1)
-    }
+	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
 }
