ceff5bb9453ad663cff5fb65a7810575c8bf06b4
Ryosuke Sasaki <nu.ciger@gmail.com>
2017-09-24 16:40:07 +0900

    add cat cmd to read HISTFILE

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -18,6 +20,7 @@ var RootCmd = &cobra.Command{
 func init() {
 	cobra.OnInitialize()
 	RootCmd.AddCommand(versionCmd)
+	RootCmd.AddCommand(catCmd)
 }
 
 var versionCmd = &cobra.Command{
@@ -26,5 +29,19 @@ var versionCmd = &cobra.Command{
 	Long:  `All software has versions. This is histag's`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("histag v1.0")
+	},
+}
+
+var catCmd = &cobra.Command{
+	Use:   "cat",
+	Short: "Print the version number of histag",
+	Long:  `All software has versions. This is histag's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		histfile := os.Getenv("HISTFILE")
+		history, err := ioutil.ReadFile(histfile)
+		if err != nil {
+			// エラー処理
+		}
+		fmt.Println(string(history))
 	},
 }
