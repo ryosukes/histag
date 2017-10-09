b0169a87d9bdd582deffc3ee563c42a30cbdcfe3
ryosukes <nu.ciger@gmail.com>
2017-10-09 16:39:57 +0900

    add cmd file and delete unnecessary cmd

---
+++ cmd/add.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(addCmd)
+}
+
+var addCmd = &cobra.Command{
+	Use:   "add",
+	Short: "Print the version number of histag",
+	Long:  `All software has versions. This is histag's`,
+	Run:   add,
+}
+
+func add(cmd *cobra.Command, args []string) {
+	histfile := os.Getenv("HISTFILE")
+	os.OpenFile(histfile, os.O_APPEND|os.O_WRONLY, 0600)
+	history, err := ioutil.ReadFile(histfile)
+
+	content := []byte("#add test")
+	ioutil.WriteFile(histfile, content, os.ModePerm)
+	if err != nil {
+		// エラー処理
+	}
+	fmt.Println(string(history))
+}
--- cmd/root.go
+++ cmd/root.go
@@ -2,12 +2,11 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
+// RootCmd is root for histag
 var RootCmd = &cobra.Command{
 	Use:   "histag",
 	Short: "This tool is pretty cool.",
@@ -20,7 +19,6 @@ var RootCmd = &cobra.Command{
 func init() {
 	cobra.OnInitialize()
 	RootCmd.AddCommand(versionCmd)
-	RootCmd.AddCommand(catCmd)
 }
 
 var versionCmd = &cobra.Command{
@@ -29,19 +27,5 @@ var versionCmd = &cobra.Command{
 	Long:  `All software has versions. This is histag's`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("histag v1.0")
-	},
-}
-
-var catCmd = &cobra.Command{
-	Use:   "cat",
-	Short: "Print the version number of histag",
-	Long:  `All software has versions. This is histag's`,
-	Run: func(cmd *cobra.Command, args []string) {
-		histfile := os.Getenv("HISTFILE")
-		history, err := ioutil.ReadFile(histfile)
-		if err != nil {
-			// エラー処理
-		}
-		fmt.Println(string(history))
 	},
 }
